459cb5d93db16f012be92794a7307f366d679753
Andrei Ignat <ignatandrei@yahoo.com>
2024-04-13 12:40:20 +0300

    cmd

---
+++ src/runAll.bat
@@ -0,0 +1,3 @@
+wt  -w 1 nt -d generator/PatternsConsole  -c "cmd" 
+rem wt new-tab Powershell -d generator/PatternsConsole -c "dotnet watch run --no-hot-reload" 
+rem ; new-tab Powershell -d patternsSite -c npm start
