064882ab70ecbba8b475366b7c3642038bff385d
Andrei Ignat <ignatandrei@yahoo.com>
2024-04-13 08:20:46 +0300

    start builder

---
+++ src/examples/builder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConnectionString();
+            UriMod();
+        }
+
+        private static void UriMod()
+        {
+            //start example 1
+            var uri = new Uri("https://msprogrammer.serviciipeweb.ro/category/friday-links/");
+            var b = new UriBuilder(uri);
+            //changing part
+            b.Scheme = "http";
+            //now we have http://msprogrammer.serviciipeweb.ro/category/friday-links/
+            Console.WriteLine(b.Uri);
+            //changing part
+            b.Path = "2018/03/05/design-patterns-class/";
+            //now we have http://msprogrammer.serviciipeweb.ro/2018/03/05/design-patterns-class/
+            Console.WriteLine(b.Uri);
+            //end example 1
+        }
+
+        static void ConnectionString()
+        {
+            var build = new SqlConnectionStringBuilder();
+            build.DataSource = ".";
+            build.InitialCatalog = "MyDatabase";
+            build.ConnectTimeout = 30;
+            Console.WriteLine(build.ConnectionString);
+        }
+    }
+}
--- src/generator/PatternsData/PatternData.cs
+++ src/generator/PatternsData/PatternData.cs
@@ -5,10 +5,14 @@ public class PatternData
     public int Id { get; set; }
     public string Title { get; set; }=string.Empty;
     public string Description { get; set; } = string.Empty;
-//20. Definition Wikipedia
-//25. Implementation definition in framework
-//30. How can I use the implementation from.NET 
-//40. Practical Use Cases
-//50. Homework
+
+    public string LinkWikipedia { get; set; } = string.Empty;
+    public string DemoFileCsproj { get; set; } = string.Empty;
+    public string Tags { get; set; } = string.Empty;
+    public string ClassNames { get; set; } = string.Empty;
+    public string Homework { get; set; } = string.Empty;
+    //30. How can I use the implementation from.NET 
+    //40. Practical Use Cases
+
 
 }
