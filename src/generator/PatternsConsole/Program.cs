06a4d6796f5ed1ea5fb775f50d1f729c1d0f153c
Andrei Ignat <ignatandrei@yahoo.com>
2024-04-19 08:07:09 +0300

    write on blog already

--- src/generator/PatternsConsole/Program.cs
+++ src/generator/PatternsConsole/Program.cs
@@ -5,7 +5,7 @@ var all =await cnt.PatternData.ToArrayAsync();
 GeneratorFiles generator=new(all);
 await generator.Initialize(Path.Combine(rootFolderFromGit,"src","examples"));
 await generator.WriteDocusaurus(Path.Combine(rootFolderFromGit, "src","patternsSite","docs"));
-await generator.WriteBlog(4);
+await generator.WriteBlog(12);
 await generator.WriteReadme(Path.Combine(rootFolderFromGit,"README.md"));
 Console.WriteLine("done at " + DateTime.Now.ToString("HH:mm"));
 
