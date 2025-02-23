99f3cabefc9a0e1d928d9de2c84a93c71f5d3937
Andrei Ignat <ignatandrei@yahoo.com>
2025-02-23 21:07:12 +0200

    composite provider

---
+++ src/examples/CompositeProvider/CompositeProvider/CompositeProviderDemo.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.Extensions.FileProviders;
+
+namespace CompositeProvider;
+public class CompositeProviderDemo
+{
+    private readonly IFileProvider _fileProvider;
+    public CompositeProviderDemo(string folder)
+    {
+        var fileOnHDDProvider = new PhysicalFileProvider(folder);
+        var manifestEmbeddedProvider =
+    new ManifestEmbeddedFileProvider(this.GetType().Assembly);
+        _fileProvider = new CompositeFileProvider(fileOnHDDProvider,manifestEmbeddedProvider);
+
+    }
+    public string[] GetFiles()
+    {
+        List<string> ret = [];
+        var files = _fileProvider.GetDirectoryContents(string.Empty);
+        var contents = _fileProvider.GetDirectoryContents(string.Empty);
+        foreach (var fileInfo in contents)
+        {
+            ret.Add(fileInfo.Name);
+        }
+        return ret.ToArray();
+    }
+    public string GetContentFile(string name)
+    {
+        var fileInfo = _fileProvider.GetFileInfo(name);
+        if (fileInfo.Exists)
+        {
+            using var stream = fileInfo.CreateReadStream();
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+        return string.Empty;
+    }
+
+}
---
+++ src/examples/CompositeProvider/CompositeProvider/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompositeProvider;
+
+Console.WriteLine("Hello, World!");
+var path = Directory.GetParent(Environment.CurrentDirectory);
+ArgumentNullException.ThrowIfNull(path);
+CompositeProviderDemo compositeProvider = new(path.FullName) ;
+Console.WriteLine("Files in the current directory:");
+foreach (var file in compositeProvider.GetFiles())
+{
+    Console.WriteLine(file);
+}
+Console.WriteLine("obtain MyResource.txt even if does not exists on folder");
+Console.WriteLine(compositeProvider.GetContentFile("MyResource.txt"));
